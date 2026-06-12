332fb7b7941cec18f1cdcad30d0d34d4bc3b5d7a
Zak Griffin <zakgriffin1209@gmail.com>
2026-06-12 18:26:19 -0400

    prep for cloudlfare pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+/// <reference types="node" />
 import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => ({
-    base: command === "build" ? "/ie-design-website/" : "/",
+    base: command === "build" && !process.env.CF_PAGES ? "/ie-design-website/" : "/",
 }));
