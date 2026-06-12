465b84fb3bf9c51924bd4c5c22733b7a8e670d92
Zak Griffin <zakgriffin1209@gmail.com>
2026-06-12 19:01:41 -0400

    remove vite config and gh-pages, clean up for Cloudflare Pages

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { defineConfig } from "vite";
-
-export default defineConfig(({ command }) => ({
-    base: command === "build" && !process.env.CF_PAGES ? "/ie-design-website/" : "/",
-    plugins: [],
-}));
